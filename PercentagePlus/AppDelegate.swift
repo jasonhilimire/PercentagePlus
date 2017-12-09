a15b12827d071dd3a8043f99637ba6a0acc0e5e5
jasonhilimire <jasonhilimire@mac.com>
2017-12-08 17:48:06 -0700

    Setup to add share/add 3d touch actions still need to implement the actual ability to share (& remove the alert controller)

--- PercentagePlus/AppDelegate.swift
+++ PercentagePlus/AppDelegate.swift
@@ -12,7 +12,53 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    /// Saved shortcut item used as a result of an app launch, used later when app is activated.
+    var launchedShortcutItem: UIApplicationShortcutItem?
+    
+    // enum for handling of shortcut identifier
+    enum ShortCutIdentifier: String {
+        case Share
+        case Add
+        
+        init?(fullType: String) {
+            guard let last = fullType.components(separatedBy: ".").last else { return nil }
+            self.init(rawValue: last)
+        }
+        
+        var type: String {
+            return Bundle.main.bundleIdentifier! + ".\(self.rawValue)"
+        }
+    }
 
+    // method for handling UIApplicationShortcutItem
+    func handleShortCutItem(shortcutItem: UIApplicationShortcutItem) -> Bool {
+        var handled = false
+        //Verify that the provided shortcutITem's 'type' is one handled by the application
+        guard ShortCutIdentifier(fullType: shortcutItem.type) != nil else { return false }
+        guard let shortCutType = shortcutItem.type as? String? else { return false }
+        
+        switch (shortCutType) {
+        case ShortCutIdentifier.Share.type?:
+            //handle shortcut 1 (Static)
+            handled = true
+        break
+        case ShortCutIdentifier.Add.type?:
+            //handle shortcut 2 (Static)
+            handled = true
+        break
+        default:
+            break
+        }
+        
+        // Consturt an alert using the details of the shortcut used to open the app
+        let alertController = UIAlertController(title: "Shortcut Handled", message :"\(shortcutItem.localizedTitle)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        //Disply an alert indicatin the shortcut selected from the home screen
+        window!.rootViewController?.present(alertController, animated: true, completion: nil)
+        return handled
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -35,10 +81,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        guard let shortcut = launchedShortcutItem else { return }
+        _ = handleShortCutItem(shortcutItem: shortcut)
+        launchedShortcutItem = nil
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        let handledShortCutItem = handleShortCutItem(shortcutItem: shortcutItem)
+        completionHandler(handledShortCutItem)
     }
 
 
